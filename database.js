6448cb7eebdcd892bea81e38e39b2aff5f34a27e
Mollycold <wutthisak4450@gmail.com>
2025-02-20 14:48:03 +0700

    Add files via upload

---
+++ database.js
@@ -0,0 +1,20 @@
+// database.js
+const mysql = require('mysql2');
+
+// สร้างการเชื่อมต่อไปยังฐานข้อมูล MySQL
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mini_project_db'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = db;
